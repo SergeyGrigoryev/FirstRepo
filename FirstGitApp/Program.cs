fbefd3bbeaa602db605d686feeb0a90eb72eec47
SergeyGrigoryev <grigoryevsn@gmail.com>
2013-02-15 00:41:21 +0600

    Change Program.cs Add message "Hello, World!"

--- FirstGitApp/Program.cs
+++ FirstGitApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FirstGitApp
 {
@@ -9,6 +6,7 @@ namespace FirstGitApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
         }
     }
 }
